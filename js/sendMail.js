79891e00cbde1b7d38daa5234a0ab29268d1828b
ks123kirill <ks123kirill@gmail.com>
2022-02-08 18:47:00 +0300

    Updates

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,3 +1,31 @@
+const ALERT_SHOW_TIME = 5000;
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.width = '500px';
+  alertContainer.style.height = '200px';
+  alertContainer.style.backgroundColor = '#353535';
+  alertContainer.style.zIndex = 1000;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.top = 0;
+  alertContainer.style.bottom = 0;
+  alertContainer.style.left = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.margin = 'auto';
+  alertContainer.style.padding = '80px 50px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.color = 'white';
+
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+
 const form = document.getElementById('form');
 form.addEventListener('submit', formSend);
 
@@ -17,8 +45,9 @@ async function formSend(evt) {
     });
     if (response.ok) {
       let result = await response.json();
-      alert(result.message);
-      form.reset;
+      showAlert(result.message);
+      // alert(result.message);
+      form.reset();
       form.classList.remove('_sending');
     }
     else {
